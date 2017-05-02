cb6cf90f81a8747054111e67f8e9a13d2b288006
TheTechnoToad <spersonjohnson@atl.yearup.org>
2017-05-02 00:37:19 -0400

    for and while loop added

--- Javascript sterf/LargestNumFinder.js
+++ Javascript sterf/LargestNumFinder.js
@@ -1,4 +1,4 @@
-var numArray = [-20,-19,-14,-15,-25];
+var numArray = [-20,-19,-14,-15,-25,-11,-12,-14,-18,-98];
 
 lNumber = numArray[0];
 
@@ -6,6 +6,16 @@ for(var i=0; i < numArray.length;i++){
   if(lNumber < numArray[i]){
     lNumber = numArray[i];
   }
+}
+
+console.log(lNumber);
+
+var i = 0;
+while(i < numArray.length){
+  if(lNumber < numArray[i]){
+    lNumber = numArray[i];
+  }
+  i++;
 }
 
 console.log(lNumber);
